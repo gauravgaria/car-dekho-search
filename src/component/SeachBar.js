c1757ddd82a5616e4518f8137a73316f0be444ba
Gaurav <gaurav.garia93@gmail.com>
2021-08-15 20:42:42 +0530

    search bar added

--- src/component/SeachBar.js
+++ src/component/SeachBar.js
@@ -1,12 +1,23 @@
 import React from "react";
 
 class SearchBar extends React.Component {
+  state = { search: "" };
+  onInputChange = (event) => {
+    this.setState({ seacrh: event.target.value });
+  };
+
   render() {
     return (
       <div className="ui loading search">
         <div className="ui icon input">
           <h3 style={{ marginTop: "12px" }}> Car Search </h3>
-          <input className="prompt" type="text" placeholder="Search for car" />
+          <input
+            className="prompt"
+            type="text"
+            value={this.state.seacrh}
+            onChange={this.onInputChange}
+            placeholder="Search for car"
+          />
         </div>
         <div className="results"></div>
       </div>
